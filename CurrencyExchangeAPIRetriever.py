11bbc41689d0b7a9ed184081907039f536191c71
Ilias Charitos <ilias4780@hotmail.com>
2022-07-01 23:03:54 +0100

    extended timeseries to up to 5 years and added best month to trade calculation

--- CurrencyExchangeAPIRetriever.py
+++ CurrencyExchangeAPIRetriever.py
@@ -1,6 +1,7 @@
 import datetime
 
 import requests
+from dateutil.relativedelta import relativedelta
 
 
 class CurrencyExchangeAPIRetriever(object):
@@ -10,10 +11,13 @@ class CurrencyExchangeAPIRetriever(object):
     :param api_key: API unique key, required to authenticate for the requests.
     :type api_key: str
     """
+
+    valid_periods = [None, "1y", "2y", "3y", "5y"]
+
     def __init__(self, api_key):
         self.api_key = api_key
 
-    def get_all_available_symbols(self):
+    def retrieve_all_available_symbols(self):
         """
         Retrieves all available currency exchange symbols and their long names, and
         returns them in a dictionary format.
@@ -28,9 +32,9 @@ class CurrencyExchangeAPIRetriever(object):
 
         return response.json()["symbols"]
 
-    def get_latest_exchange_rates(self, base=None, symbols=None):
+    def retrieve_latest_exchange_rates(self, base=None, symbols=None):
         """
-        Retieves real-time exchange rate data updated every 60 minutes, every 10 minutes or every 60 seconds.
+        Retrieves real-time exchange rate data updated every 60 minutes, every 10 minutes or every 60 seconds.
         Returns a dictionary containing all data.
 
         :param base: Base three-letter currency code of your preferred base currency.
@@ -47,9 +51,9 @@ class CurrencyExchangeAPIRetriever(object):
                 raise Exception(f"Failed to retrieve latest exchange rates, with error status: {response.status_code}.")
             return response.json()
         else:
-            print("Either base or symbols argument was None. Please pass both arguments.")
+            raise Exception("Either base or symbols argument was None. Please pass both arguments.")
 
-    def get_timeseries_of_exchange_rates(self, start_date=None, end_date=None, base=None, symbols=None):
+    def _retrieve_timeseries_of_exchange_rates(self, start_date=None, end_date=None, base=None, symbols=None):
         """
         Retrieves a timeseries of exchange rates between the base currency and the symbols. Start and end date should
         not be more than 365 days different.
@@ -64,17 +68,73 @@ class CurrencyExchangeAPIRetriever(object):
         :type symbols: str
         :return: dict
         """
-        if all([start_date, end_date, base, symbols]):
-            if datetime.datetime.strptime(end_date, "%Y-%m-%d") - datetime.datetime.strptime(start_date, "%Y-%m-%d") > \
-              datetime.timedelta(days=365):
-                print("Days between end date and start date is larger than 365 days. Please select valid dates.")
-            else:
-                url = f"https://api.apilayer.com/fixer/timeseries?symbols={symbols}&base={base}" \
-                      f"&start_date={start_date}&end_date={end_date}"
-                headers = {"apikey": self.api_key}
-                response = requests.request("GET", url, headers=headers)
-                if response.status_code != 200:
-                    raise Exception(f"Failed to retrieve latest exchange rates, with error status: {response.status_code}.")
-                return response.json()
+        url = f"https://api.apilayer.com/fixer/timeseries?symbols={symbols}&base={base}" \
+              f"&start_date={start_date}&end_date={end_date}"
+        headers = {"apikey": self.api_key}
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve latest exchange rates, with error status: {response.status_code}.")
+        return response.json()
+
+    def get_timeseries_of_exchange_rates(self, start_date=None, end_date=None, period=None, base=None, symbols=None):
+        """
+        Get a timeseries of exchange rates of the base currency against the symbols, between the start and end
+        date, or for the period specified. Returns a dict containing the results.
+
+        :param start_date: Start date for the timeseries retrieval. Should follow YYYY-MM-DD format.
+        :type start_date: str
+        :param end_date: End date for the timeseries retrieval. Should follow YYYY-MM-DD format.
+        :type end_date: str
+        :param period: Time period for retrieving data. Valid periods: ["1y", "2y", "3y", "5y"]
+        :type period: str
+        :param base: Base three-letter currency code of your preferred base currency.
+        :type base: str
+        :param symbols: String of comma-separated currency codes to limit output currencies
+        :type symbols: str
+        :return: dict
+        """
+        # check how many requests are needed based on the number of years of the time period requested
+        if period:
+            number_of_years = int(period[0])
+        elif start_date and end_date:
+            number_of_years = relativedelta(datetime.datetime.strptime(end_date, "%Y-%m-%d"),
+                                            datetime.datetime.strptime(start_date, "%Y-%m-%d")).years + 1
         else:
-            print("At least one of the arguments was None. Please provide all arguments.")
+            raise Exception("Please provide valid arguments. Provide a period or both start and end date.")
+
+        # based on the number of years, make the necessary amount of requests
+        if number_of_years <= 1:
+            if period:
+                date_today = datetime.datetime.today()
+                return self._retrieve_timeseries_of_exchange_rates(
+                    (date_today - relativedelta(years=1)).strftime("%Y-%m-%d"), date_today.strftime("%Y-%m-%d"),
+                    base, symbols
+                )
+            else:
+                return self._retrieve_timeseries_of_exchange_rates(start_date, end_date, base, symbols)
+        else:
+            data_retrieved = []
+            date_today = datetime.datetime.today()
+            for year in range(number_of_years):
+                if period:
+                    end_date_req = (date_today - relativedelta(years=year)).strftime("%Y-%m-%d")
+                    start_date_req = (date_today - relativedelta(years=year + 1)).strftime("%Y-%m-%d")
+                else:
+                    end_date_req = (datetime.datetime.strptime(end_date, "%Y-%m-%d") -
+                                    relativedelta(years=year)).strftime("%Y-%m-%d")
+                    start_date_req = (datetime.datetime.strptime(end_date, "%Y-%m-%d") -
+                                      relativedelta(years=year + 1)).strftime("%Y-%m-%d")
+                    if start_date_req < start_date:
+                        start_date_req = start_date
+                data_retrieved.append(self._retrieve_timeseries_of_exchange_rates(
+                    start_date_req, end_date_req, base, symbols))
+            # post process the requests
+            results_dict = dict()
+            results_dict["start_date"] = data_retrieved[-1]["start_date"]
+            results_dict["end_date"] = data_retrieved[0]["end_date"]
+            for item in reversed(data_retrieved):
+                if "rates" not in results_dict:
+                    results_dict["rates"] = item["rates"]
+                else:
+                    results_dict["rates"].update(item["rates"])
+            return results_dict
--- dashboard.py
+++ dashboard.py
@@ -3,10 +3,87 @@ from datetime import datetime, timedelta
 
 import pandas as pd
 import streamlit as st
-from bokeh.models import HoverTool
+from bokeh.models import DatetimeTickFormatter, HoverTool
 from bokeh.plotting import figure
+from calendar import month_name
+from dateutil.relativedelta import relativedelta
 
 from CurrencyExchangeAPIRetriever import CurrencyExchangeAPIRetriever
+
+
+def calculate_best_months(timeseries_dict, symbol):
+    """
+    Calculates the best months to exchange your base currency to your target currency. Results are based on
+    the best average rates per month of the requested period.
+
+    :param timeseries_dict: Dictionary containing the retrieved timeseries
+    :type timeseries_dict: dict
+    :param symbol: Currency symbols
+    :type symbol: str
+    :return: pd.Dataframes
+    """
+    df = pd.DataFrame(
+        list(zip(timeseries_dict["rates"].keys(),
+                 [item[symbol] for item in timeseries_dict["rates"].values()])),
+        columns=["Datetime", "Rate"])
+    # convert type of datetime column to datetime
+    df["Datetime"] = pd.to_datetime(df["Datetime"], format="%Y-%m-%d")
+    # group by and find the average per month
+    df = df.groupby(pd.Grouper(freq='M', key="Datetime")).mean().reset_index()
+    # convert type of datetime index to string again using only the month
+    df["Datetime"] = df["Datetime"].dt.strftime('%B')
+    df = df.rename(columns={'Datetime': 'Month'})
+    # group by and find the average per month in total
+    df = df.groupby(by="Month").mean().reset_index()
+    # sort based on months
+    months = list(month_name)
+    df['Month'] = pd.Categorical(df['Month'], categories=months, ordered=True)
+    df.sort_values(by="Month", inplace=True)
+    return df
+
+
+def plot_charts(base_currency_t, selected_symbols_list_t, timeseries_dict):
+    """
+    Plots the timeseries and best months charts for all target currency symbols.
+
+    :param base_currency_t: Base three-letter currency code of your preferred base currency.
+    :type base_currency_t: str
+    :param selected_symbols_list_t: Currency target symbols
+    :type selected_symbols_list_t: list
+    :param timeseries_dict: Dictionary containing the retrieved timeseries
+    :type timeseries_dict: dict
+    """
+    for symbol in selected_symbols_list_t:
+        # timeseries chart
+        p = figure(
+            title=f"Line chart showing the timeseries currency exchange rates of {base_currency_t} to the "
+                  f"requested currency: {symbol}.",
+            x_axis_label="Date",
+            x_axis_type="datetime",
+            y_axis_label=f"{base_currency_t} -> {symbol} rate"
+        )
+        p.line([datetime.strptime(date, "%Y-%m-%d") for date in timeseries_dict["rates"].keys()],
+               [item[symbol] for item in timeseries_dict["rates"].values()], legend_label='Trend', line_width=2)
+        p.add_tools(HoverTool(tooltips=[("Rate", "@y"), ("Date", "@x{%Y-%m-%d}")],
+                              formatters={"@x": "datetime"}, mode="vline"))
+        st.bokeh_chart(p, use_container_width=True)
+
+        # best month chart
+        best_months_df = calculate_best_months(timeseries_dict, symbol)
+        p2 = figure(
+            title=f"Line chart showing the best months to trade in average by using "
+                  f"data for the selected time interval.",
+            x_axis_label="Month",
+            x_axis_type="datetime",
+            y_axis_label=f"{base_currency_t} -> {symbol} rate"
+        )
+        p2.line([datetime.strptime(item, "%B") for item in best_months_df["Month"].to_list()],
+                list(map(float, best_months_df["Rate"].to_list())),
+                legend_label='Trend', line_width=2)
+        p2.xaxis[0].formatter = DatetimeTickFormatter(months="%B")
+        p2.add_tools(HoverTool(tooltips=[("Rate", "@y"), ("Date", "@x{%B}")],
+                               formatters={"@x": "datetime"}, mode="vline"))
+        st.bokeh_chart(p2, use_container_width=True)
 
 
 def run_dashboard():
@@ -26,7 +103,7 @@ def run_dashboard():
     st.header("Currency Symbols and long names")
     if "all_symbols" not in st.session_state:
         with st.spinner('Retrieving..'):
-            st.session_state.all_symbols = retriever.get_all_available_symbols()
+            st.session_state.all_symbols = retriever.retrieve_all_available_symbols()
         st.session_state.all_symbols_df = pd.DataFrame(list(zip(st.session_state.all_symbols.keys(),
                                                                 st.session_state.all_symbols.values())),
                                                        columns=["Symbol", "Long Name"])
@@ -42,8 +119,8 @@ def run_dashboard():
     if st.button("Finished selection, retrieve rates"):
         if base_currency_l and selected_symbols_list_l:
             with st.spinner('Retrieving..'):
-                latest_rates = retriever.get_latest_exchange_rates(base=base_currency_l,
-                                                                   symbols=','.join(selected_symbols_list_l))
+                latest_rates = retriever.retrieve_latest_exchange_rates(base=base_currency_l,
+                                                                        symbols=','.join(selected_symbols_list_l))
             st.text(f"Rates last updated at "
                     f"{datetime.fromtimestamp(latest_rates['timestamp']).strftime('%Y-%m-%d %H:%M:%S')}")
             for symbol, value in latest_rates["rates"].items():
@@ -53,42 +130,55 @@ def run_dashboard():
     st.text("--------------------------------------------------------------------------------------------------------")
 
     # retrieve timeseries of exchange rates
-    st.header("Exchange Rates Timeseries")
+    st.header("Exchange Rates Timeseries and Best Months to Trade charts")
     base_currency_t = st.selectbox("Base currency symbol", st.session_state.all_symbols.keys(),
                                    index=list(st.session_state.all_symbols.keys()).index("GBP"), key="timeseries_sb")
     selected_symbols_list_t = st.multiselect("Currency symbols to limit output currencies",
                                              st.session_state.all_symbols.keys(), default=None, key="timeseries_msb")
+    st.markdown("""
+    Please select either a `Time period` from below or a `Start date` and an `End date`. If you select both, then the 
+    `Time period` will be used, and not the `Start date` and `End date`.
+    
+    N.B. Fixer API allows to retrieve a timeseries of only one year. If you have selected a time period or dates that
+    span longer than a year, then multiple requests will be made. Please be cautious around the number of requests
+    you have available via your API subscription. (e.g. if you have selected 3 target currencies and 5y span, then 15
+    requests will be made)
+    """)
+    time_period = st.selectbox("Time period", CurrencyExchangeAPIRetriever.valid_periods)
     end_date = st.date_input("End date of timeseries",
                              value=datetime.today(),
                              max_value=datetime.today(),
-                             help="Must be later than start date, but no more than 365 days.")
+                             help="Must be later than start date.")
     start_date = st.date_input("Start date of timeseries",
                                value=end_date - timedelta(days=1),
-                               min_value=end_date - timedelta(days=365),
+                               min_value=end_date - relativedelta(years=5),
                                max_value=end_date - timedelta(days=1),
-                               help="Must be earlier than end date, but no more than 365 days.")
+                               help="Must be earlier than end date.")
     if st.button("Finished selection, retrieve timeseries"):
-        if base_currency_t and selected_symbols_list_t:
-            with st.spinner('Retrieving..'):
-                timeseries_dict = retriever.get_timeseries_of_exchange_rates(start_date=start_date.strftime("%Y-%m-%d"),
-                                                                             end_date=end_date.strftime("%Y-%m-%d"),
-                                                                             base=base_currency_t,
-                                                                             symbols=','.join(selected_symbols_list_t))
-            for symbol in selected_symbols_list_t:
-                p = figure(
-                    title=f"Line chart showing the timeseries currency exchange rates of {base_currency_t} to the "
-                          f"requested currency: {symbol}.",
-                    x_axis_label="Date",
-                    x_axis_type="datetime",
-                    y_axis_label=f"{base_currency_t} -> {symbol} rate"
-                )
-                p.line([datetime.strptime(date, "%Y-%m-%d") for date in timeseries_dict["rates"].keys()],
-                       [item[symbol] for item in timeseries_dict["rates"].values()], legend_label='Trend', line_width=2)
-                p.add_tools(HoverTool(tooltips=[("Rate", "@y"), ("Date", "@x{%Y-%m-%d}")],
-                                      formatters={"@x": "datetime"}, mode="vline"))
-                st.bokeh_chart(p, use_container_width=True)
+        if selected_symbols_list_t:
+            if (start_date and end_date) or time_period:
+                with st.spinner('Retrieving..'):
+                    if time_period:
+                        if (base_currency_t, selected_symbols_list_t, time_period) not in st.session_state:
+                            st.session_state[(base_currency_t, selected_symbols_list_t, time_period)] = \
+                                retriever.get_timeseries_of_exchange_rates(
+                                    period=time_period,
+                                    base=base_currency_t,
+                                    symbols=','.join(selected_symbols_list_t))
+                        plot_charts(base_currency_t, selected_symbols_list_t,
+                                    st.session_state[(base_currency_t, selected_symbols_list_t, time_period)])
+                    else:
+                        if (base_currency_t, selected_symbols_list_t, start_date, end_date) not in st.session_state:
+                            st.session_state[(base_currency_t, selected_symbols_list_t, start_date, end_date)] = \
+                                retriever.get_timeseries_of_exchange_rates(
+                                        start_date=start_date.strftime("%Y-%m-%d"),
+                                        end_date=end_date.strftime("%Y-%m-%d"),
+                                        base=base_currency_t,
+                                        symbols=','.join(selected_symbols_list_t))
+                        plot_charts(base_currency_t, selected_symbols_list_t,
+                                    st.session_state[(base_currency_t, selected_symbols_list_t, start_date, end_date)])
         else:
-            st.warning("No target currency symbols have been selected. Please select at least one.")
+            st.error("No target currency symbols have been selected. Please select at least one.")
 
 
 if __name__ == '__main__':
